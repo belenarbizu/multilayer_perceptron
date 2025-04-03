a6b83a8a33e6e53d018f96cf0fa58db859faaaac
belen-applymed <158149022+belen-applymed@users.noreply.github.com>
2025-04-03 17:06:49 +0200

    layer init

---
+++ layer.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+import numpy as np
+
+class Layer:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = np.random.randn(n_inputs, n_neurons) * 0.01
+        self.biases = np.zeros((1, n_neurons))
+        self.output = None
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+        return self.output
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
