a6c7584da3a1a3d15b2347f444961e7c551c4f6d
barbizu <belenarbizu98@gmail.com>
2025-05-05 17:50:54 +0200

    split_data docstrings

--- split_data.py
+++ split_data.py
@@ -4,6 +4,16 @@ import pandas as pd
 import sys
 
 def split_dataset(data):
+    '''
+    Loads a dataset, replaces NaN values, and splits it into training and validation sets.
+
+    Parameters:
+    data (str): Path to the dataset file (CSV format).
+
+    Returns:
+        pd.DataFrame: Training set (80% of the data).
+        pd.DataFrame: Validation set (20% of the data).
+    '''
     data = DataParser.replace_nan_values(DataParser.open_file(data, None))
 
     np.random.seed(42)
@@ -23,9 +33,22 @@ def split_dataset(data):
     return train_set, valid_set
 
 def main():
+    '''
+    Entry point of the script. Validates command-line arguments,
+    splits the dataset into training and validation sets,
+    and saves them as CSV files.
+
+    Usage:
+    python3 ./split_data.py dataset_name
+
+    Output:
+    - train.csv: File containing the training data.
+    - validation.csv: File containing the validation data.
+    '''
     if (len(sys.argv) != 2):
         print("Usage: python3 ./split_data.py dataset_name")
         sys.exit(1)
+
     train, test = split_dataset(sys.argv[1])
     train.to_csv("train.csv", index=False)
     test.to_csv("validation.csv", index=False)
